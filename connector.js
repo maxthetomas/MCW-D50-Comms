a93c72ef58aa8db94149db3cd1529e6bbb900da2
maxthetomas <nasadregistr@gmail.com>
2023-01-17 17:55:38 +0300

    Connector now saves text instead of base64

--- connector.js
+++ connector.js
@@ -1,7 +1,8 @@
-require('dotenv').config();
+import { config } from 'dotenv';
 import { createConnection } from 'node:net';
 import { writeFileSync } from 'node:fs';
 
+config();
 let full_data = [];
 let connected = null;
 
@@ -14,14 +15,14 @@ const connection = createConnection({
 })
 
 connection.on('data', (d) => { 
-  full_data.push(new Date().toISOString() + " < " + d.toString('base64'));
+  full_data.push(new Date().toISOString() + " < " + d.toString());
   const data = d.toString().replace(/\r\n/g, '\n').replace(/\r/g, '\n');
   console.log(data);
 })
 
 process.stdin.addListener('data', (e) => { 
   connection.write(e); 
-  full_data.push(new Date().toISOString() + " > " + e.toString('base64'))
+  full_data.push(new Date().toISOString() + " > " + e.toString())
 })
 process.stdin.read();
 
@@ -29,7 +30,7 @@ process.on('SIGINT', () => {
   console.log("Caught interrupt signal, stopping.");
 
   connection.destroy();
-  writeFileSync(`logs/${new Date().toISOString()}.txt`, `Connection from ${connected.toISOString()}. MSGS: ${full_data.length} \n` + full_data.join('\n'));
+  writeFileSync(`logs/${new Date().toISOString().replace(/\:/g,'-')}.txt`, `Connection from ${connected.toISOString()}. MSGS: ${full_data.length} \n` + full_data.join('\n'));
 
   process.exit();
 });
